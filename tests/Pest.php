d01d10725a8b23ae688dd5dec6b4755c91b63106
Remzi Kocak <kocak0068@gmail.com>
2023-10-25 15:25:48 +0300

    Configure Tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,3 +1,20 @@
 <?php
 
 declare(strict_types=1);
+
+use Illuminate\Support\Facades\Http;
+use RKocak\Vallet\Payment;
+use RKocak\Vallet\Tests\TestCase;
+
+uses(TestCase::class)->in(__DIR__.'/Feature');
+
+function forceValletResponse(string $message = 'An error occurred', int $status = 500): void
+{
+    Http::fake([
+        Payment::VALLET_URL => Http::response([
+            'status'           => 'error',
+            'errorMessage'     => $message,
+            'payment_page_url' => 'https://www.vallet.com.tr/fake-payment-page-url',
+        ], $status),
+    ]);
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,4 +8,8 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
 }
