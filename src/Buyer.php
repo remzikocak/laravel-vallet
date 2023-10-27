fe552b3c4707da51bff936f3112a3f0f5e8f729c
Remzi Kocak <kocak0068@gmail.com>
2023-10-27 15:56:04 +0300

    Add: `getFullName` to `Buyer` class

--- src/Buyer.php
+++ src/Buyer.php
@@ -150,6 +150,11 @@ class Buyer implements Arrayable
         return $this->surname;
     }
 
+    public function getFullName(): string
+    {
+        return $this->getName().' '.$this->getSurname();
+    }
+
     public function toArray(): array
     {
         return [
