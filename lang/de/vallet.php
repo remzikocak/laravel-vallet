727d2fd9a72de6e26d09e4fe199d47e436e3fc0f
Remzi Kocak <kocak0068@gmail.com>
2023-10-27 13:34:20 +0300

    Add: german translation

---
+++ lang/de/vallet.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'paymentType' => [
+        'KART'         => 'Kredit-, Debitkarte',
+        'BANKA_HAVALE' => 'Banküberweisung',
+        'YURT_DISI'    => 'Kredit-, Debitkarte (Ausland)',
+    ],
+];
