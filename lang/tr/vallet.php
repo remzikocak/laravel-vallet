d2f2d2f6d616d8ee2546765a76f89554caf389c9
Remzi Kocak <kocak0068@gmail.com>
2023-10-27 13:34:28 +0300

    Add: turkish translations

---
+++ lang/tr/vallet.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'paymentType' => [
+        'KART'         => 'Kredi / Banka Kartı',
+        'BANKA_HAVALE' => 'Havale / EFT',
+        'YURT_DISI'    => 'Yurt Dışı Kredi Kartı',
+    ],
+];
