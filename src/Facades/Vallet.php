8c80a28d1cc2fd28bb60a429d69e3b5485afbd1e
Remzi Kocak <kocak0068@gmail.com>
2023-10-27 14:14:37 +0300

    Add: Missing Docblock

--- src/Facades/Vallet.php
+++ src/Facades/Vallet.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace RKocak\Vallet\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use RKocak\Vallet\Contracts\{PaymentContract, RefundContract, ValletContract};
+use RKocak\Vallet\Contracts\{PaymentContract, RefundContract, ResponseContract, ValletContract};
 
 /**
  * @method static PaymentContract createPayment()
  * @method static RefundContract createRefund()
+ * @method static ResponseContract getResponse()
  */
 class Vallet extends Facade
 {
